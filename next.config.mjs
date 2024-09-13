3b7f56d402ba8d8195fdcec0e3e04716de793c68
MOHAMMAD MUJAHID <72245261+mujju834@users.noreply.github.com>
2024-09-12 22:15:16 -0400

    my qr code generator-second one

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'standalone', // Important for deployment
+  }
+  
+  export default nextConfig;
+  
